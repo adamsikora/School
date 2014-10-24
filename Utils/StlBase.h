be8d7a636487bd72ce3e8c9eba3e0a2a0c481a6b
adamsikora <adam.sikora73@gmail.com>
2014-10-24 11:54:08 +0200

    start

---
+++ Utils/StlBase.h
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <vector>
+#include <set>
+#include <map>
+#include <bitset>
+
+#include <string>
+#include <sstream>
+#include <iostream>
+#include <iomanip>
+
+#include <memory>
+#include <functional>
+
+#include <random>
+
+#include <limits>
+
+#include <time.h>
+
+#include <math.h>
